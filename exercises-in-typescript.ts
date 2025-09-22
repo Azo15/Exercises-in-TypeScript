431e339592e78f876b1d7bd6a3ef69e8f0c3b0d9
Azo İsmail <ismailazo260@gmail.com>
2025-09-22 19:51:40 +0300

    Create exercises-in-typescript.ts

---
+++ exercises-in-typescript.ts
@@ -0,0 +1,53 @@
+// exercises-in-typescript.ts
+
+let num1: number;
+let num2: number;
+let age: number;
+
+// ************************************************************
+// Kullanıcıdan alınan iki sayının çarpımının işaretini yazdırır
+// ************************************************************
+
+// num1 = parseInt(prompt("Birinci sayıyı giriniz")!);
+// num2 = parseInt(prompt("İkinci sayıyı giriniz")!);
+
+// if (num1 * num2 < 0) {
+//   console.log("Çarpım sonucu negatiftir");
+// } else if (num1 * num2 === 0) {
+//   console.log("Çarpım sonucu sıfıra eşittir");
+// } else {
+//   console.log("Çarpım sonucu pozitiftir");
+// }
+
+
+// ************************************************************
+// Kullanıcıdan alınan iki sayının farkını yazdırır
+// ************************************************************
+
+// num1 = parseInt(prompt("Birinci sayıyı giriniz")!);
+// num2 = parseInt(prompt("İkinci sayıyı giriniz")!);
+
+// if (num1 > num2) {
+//   console.log(num1 - num2);
+// } else {
+//   console.log(num2 - num1);
+// }
+
+
+// ************************************************************
+// Kullanıcıdan alınan yaşa göre kişinin durumunu belirler
+// ************************************************************
+
+// age = parseInt(prompt("Yaşı giriniz")!);
+
+// if (age >= 1 && age <= 2) {
+//   console.log("Bu kişi bir bebektir");
+// } else if (age >= 3 && age <= 17) {
+//   console.log("Bu kişi bir çocuktur");
+// } else if (age >= 18 && age <= 50) {
+//   console.log("Bu kişi bir yetişkindir");
+// } else if (age >= 51 && age <= 120) {
+//   console.log("Bu kişi yaşlıdır");
+// } else {
+//   console.log("Lütfen geçerli bir yaş giriniz");
+// }
